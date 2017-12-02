72c3cbe90a515bf09cca669cf01851c73c316bfc
Filip Bergkvist <filip.0717@hotmail.com>
2017-12-02 12:17:26 +0100

    Initial project

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+    private float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        transform.Translate(movement.normalized * speed * Time.deltaTime);
+	}
+}

80ce183570de43419d7907f8c61a9047d0f964a3
Filip Bergkvist <filip.0717@hotmail.com>
2017-12-02 20:25:23 +0100

    Add gif, gravel path

---
+++ Assets/Scripts/RandomTransform.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomTransform : MonoBehaviour {
+
+    [SerializeField]
+    private float maxRotation = 360f;
+    [SerializeField]
+    private float minRotation = 0f;
+
+    [SerializeField]
+    private float maxSize = 1.2f;
+    [SerializeField]
+    private float minSize = 0.8f;
+
+    void OnValidate()
+    {
+        transform.localEulerAngles = new Vector3(0f, 0f, Random.Range(minRotation, maxRotation));
+        float randomSize = Random.Range(minSize, maxSize);
+        transform.localScale = new Vector2(randomSize, randomSize);
+    }
+}

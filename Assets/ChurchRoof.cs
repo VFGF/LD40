6ff80ff73f7aeaaff320730e1b6d4a9673e64c30
Filip Bergkvist <filip.0717@hotmail.com>
2017-12-03 13:59:50 +0100

    Add church roof, lights

---
+++ Assets/ChurchRoof.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChurchRoof : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject roof;
+
+    private void Start()
+    {
+        roof.SetActive(true);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Entered church");
+        if(collision.transform.CompareTag("Player"))
+            roof.SetActive(false);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("Exited church");
+        if (collision.transform.CompareTag("Player"))
+            roof.SetActive(true);
+    }
+}
---
+++ Assets/FlickeringSize.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlickeringSize : MonoBehaviour {
+
+    [SerializeField]
+    private float min;
+    [SerializeField]
+    private float max;
+
+    private SpriteRenderer m_sr;
+    [SerializeField]
+    private int timer;
+    private int startTimer;
+
+	// Use this for initialization
+	void Start () {
+        m_sr = GetComponent<SpriteRenderer>();
+        startTimer = timer;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (timer == 0)
+        {
+            float strength = Random.Range(min, max);
+            m_sr.color = new Color(1f, 1f, 1f, Mathf.Clamp01(strength) - 0.1f);
+            transform.localScale = new Vector2(strength, strength);
+            timer = startTimer;
+        }
+        else
+        {
+            timer--;
+        }
+	}
+}

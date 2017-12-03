c419a269bad0e29abdd9847b2ff38f0d7eeea5d3
Filip Bergkvist <filip.0717@hotmail.com>
2017-12-03 19:29:37 +0100

    Added grim reaper object

---
+++ Assets/GoTowardsPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoTowardsPlayer : MonoBehaviour {
+
+    private GameObject target;
+
+	// Use this for initialization
+	void Start () {
+        target = GameObject.Find("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3.MoveTowards(transform.position, target.transform.position, )
+	}
+}

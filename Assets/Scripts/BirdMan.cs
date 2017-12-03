05ff21d6a96730895351fd860fb27166a57bf075
Filip Bergkvist <filip.0717@hotmail.com>
2017-12-03 11:02:23 +0100

    Add bird man

---
+++ Assets/Scripts/BirdMan.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdMan : MonoBehaviour {
+
+    [SerializeField]
+    private Sprite[] sprites;
+    [SerializeField]
+    private float chance;
+    [SerializeField]
+    private int index;
+    private int startIndex;
+
+    private SpriteRenderer m_sr;
+
+	// Use this for initialization
+	void Start () {
+        m_sr = GetComponent<SpriteRenderer>();
+        startIndex = index;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float rng = Random.Range(0f, 100f);
+        if(rng < chance)
+        {
+            if (index < sprites.Length - 1)
+                index++;
+            else
+                index = startIndex;
+            m_sr.sprite = sprites[index];
+        }
+	}
+}

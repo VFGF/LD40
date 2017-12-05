312812a07ee49fb459399a90734f1856ce110af8
Adam Wallberg <adamwallbergcontact@gmail.com>
2017-12-05 01:51:47 +0100

    Add game over scene

---
+++ Assets/Scripts/GameOverSceneManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverSceneManager : MonoBehaviour
+{
+	float time = 0.0f;
+
+	void Start ()
+	{
+		
+	}
+	
+	void Update ()
+	{
+		time += Time.deltaTime;
+		if(time >= 3.0f)
+		{
+			SceneManager.LoadScene("Level");
+		}
+	}
+}
